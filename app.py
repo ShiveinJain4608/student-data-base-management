487d14f00e286a3ef7e8d75ae67d10f53ed2a667
ShiveinJain4608 <shiveinjain4608@gmail.com>
2026-06-03 01:00:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
 import streamlit as st
+
+st.set_page_config(
+    page_title="Python Code Explainer",
+    page_icon="🐍",
+    layout="wide"
+)
+
+st.sidebar.title("Navigation")
+
+page = st.sidebar.radio(
+    "Select a Page",
+ [
+    "🏠 Home",
+    "💻 Source Code",
+    "📊 Code Analysis",
+    "📄 Code Summary",
+    "🔄 Program Flow",
+    "▶️ Run Game"
+]
+)
